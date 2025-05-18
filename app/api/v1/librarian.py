69d9a07cd5d59767b714f5409500cbafaccdc066
Dolgov Denis <dolgovdenis35@gmail.com>
2025-05-18 15:51:07 +0300

    api fix and main.py edit

--- app/api/v1/librarian.py
+++ app/api/v1/librarian.py
@@ -5,6 +5,6 @@ router = APIRouter(prefix="/api/v1/librarian", tags=["Librarian"])
 BOOKS = {"books": [{"title": "Red Hat"}, {"title": "1984"}]}
 
 
-@router.get("books")
+@router.get("/books")
 async def get_books() -> dict[str, list[dict[str, str]]]:
     return BOOKS
--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 import uvicorn
 from api.v1 import client_router, librarian_router
 from app_setup import setup_logger
+from database.database import database
 from fastapi import FastAPI
 from loguru import logger
 
@@ -12,13 +13,14 @@ async def lifespan(app: FastAPI):
     setup_logger()
     logger.debug("Application startup...")
     yield
+    await database.dispose()
     logger.debug("Application down.")
 
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(client_router)
 app.include_router(librarian_router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8080, app_dir="app")
+    uvicorn.run("main:app", host="127.0.0.1", port=8888, app_dir="app")
